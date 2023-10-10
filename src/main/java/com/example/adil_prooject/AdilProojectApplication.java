e34855c3536e2e53863bfabae24343192b01a210
HaruSanzu <qwertyyangsters@gmail.com>
2023-10-10 21:08:01 +0600

    Addding project to git hub repo

---
+++ src/main/java/com/example/adil_prooject/AdilProojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.adil_prooject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdilProojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdilProojectApplication.class, args);
+	}
+
+}
