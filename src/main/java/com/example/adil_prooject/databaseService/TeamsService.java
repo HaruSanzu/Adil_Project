1503f95239fa6a37f192c9887ca1a08b4a958ce5
HaruSanzu <qwertyyangsters@gmail.com>
2023-11-29 18:50:02 +0600

    Addding project to git hub repo

---
+++ src/main/java/com/example/adil_prooject/databaseService/TeamsService.java
@@ -0,0 +1,21 @@
+package com.example.adil_prooject.databaseService;
+
+import com.example.adil_prooject.databseRepository.TeamsRepository;
+import com.example.adil_prooject.models.Teams;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+@Service
+@Slf4j
+public class TeamsService {
+    private TeamsRepository teamsRepository;
+    @Autowired
+    public TeamsService(TeamsRepository teamsRepository) {
+        this.teamsRepository = teamsRepository;
+    }
+
+}
