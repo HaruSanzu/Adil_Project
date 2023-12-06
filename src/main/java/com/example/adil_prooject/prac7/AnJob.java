78764b24fdc66ee5251106a5f5fb5c6c3e058624
HaruSanzu <qwertyyangsters@gmail.com>
2023-12-06 20:18:35 +0600

    Addding project to git hub repo

---
+++ src/main/java/com/example/adil_prooject/prac7/AnJob.java
@@ -0,0 +1,11 @@
+package com.example.adil_prooject.prac7;
+
+import java.util.concurrent.Callable;
+
+public class AnJob implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+
+    }
+}
